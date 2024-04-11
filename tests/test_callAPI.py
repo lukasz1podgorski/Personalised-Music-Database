dba0de4db44919781d353ec45a0875c47d552492
Łukasz <podgorski1lukasz@gmail.com>
2024-04-11 22:12:51 +0200

    Added Mock tests

--- tests/test_callAPI.py
+++ tests/test_callAPI.py
@@ -2,10 +2,21 @@ import pytest
 import callAPI as callAPI
 import unittest.mock as mock
 
-def test_get_():
-    pass
 
-#added comment
+@mock.patch("callAPI.get_token")
+def test_get_token(mock_get_token):
+    mock_get_token.return_value = "BQCnYFGFQM8nkY8DQzjgUShVv3OpnwqGjI319BLzz_hDCJKfFcuPxV4" \
+                                  "--Tkv4rJzz3p1F5rFiDsCcm_hlDjRBXYR22_jjqRUuLie_AnsI9EmInlyJPo"
+    token = callAPI.get_token()
 
-def test_post_():
-    pass
+    assert token == "BQCnYFGFQM8nkY8DQzjgUShVv3OpnwqGjI319BLzz_hDCJKfFcuPxV4" \
+                    "--Tkv4rJzz3p1F5rFiDsCcm_hlDjRBXYR22_jjqRUuLie_AnsI9EmInlyJPo"
+
+
+@mock.patch("callAPI.search_for_new_releases")
+def test_search_for_new_releases(mock_search_for_new_releases):
+
+    mock_search_for_new_releases.return_value = {"name": "Izzy and the Black Trees", "release_date": "2024-04-05"}
+    data = callAPI.search_for_new_releases(access_token="BQCnYFGFQM8nkY8DQzjgUShVv3OpnwqGjI319BLzz_hDCJKfFcuPxV4" \
+                                                        "--Tkv4rJzz3p1F5rFiDsCcm_hlDjRBXYR22_jjqRUuLie_AnsI9EmInlyJPo", country="PL")
+    assert data == {"name": "Izzy and the Black Trees", "release_date": "2024-04-05"}
